9801b4d6b57561b9867271b46f201ab601ce4883
Mikhail <man20051208@gmail.com>
2025-08-24 01:06:33 +0300

    Переделываение содержания файлов под минимально рабочий вариант

--- conftest.py
+++ conftest.py
@@ -1,6 +1,14 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+import time
+import math
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default="en", help="Select the browser language")
--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,14 @@
-
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+import time
+import math
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
 
 
